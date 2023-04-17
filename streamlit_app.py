064eb83e18bbe48480fdfbc0c12d9fc50c5e0746
Cobalt-River <meteo54@gmail.com>
2023-04-17 17:52:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,6 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
- data = [
-    {"Name": "John", "Age": 25, "City": "New York"},
-    {"Name": "Jane", "Age": 30, "City": "Chicago"},
-    {"Name": "Bob", "Age": 35, "City": "San Francisco"}
-]
+data = [{"Name": "John", "Age": 25, "City": "New York"},{"Name": "Jane", "Age": 30, "City": "Chicago"},{"Name": "Bob", "Age": 35, "City": "San Francisco"}]
 st.table(data)
 
